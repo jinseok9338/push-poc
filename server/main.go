cc887086302af7c678a77bdb68ab92c2ad4063b1
Jinseok Seo <jinseok9338@gmail.com>
2023-02-19 16:24:46 +0900

    making dev env for poc

--- server/main.go
+++ server/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	// Get the database URL from the environment variable
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("DATABASE_URL environment variable not set")
+	}
+
+	// Open a connection to the database
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create a new echo instance
+	e := echo.New()
+
+	// Define your API routes and handlers here
+	// make hello world handler
+	helloWorld := func(c echo.Context) error {
+		return c.String(200, "Hello, !!!")
+	}
+
+	// Start the server with handler
+	e.GET("/", helloWorld)
+	e.Logger.Fatal(e.Start(":3000"))
+
 }
